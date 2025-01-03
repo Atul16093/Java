e4fc0f2e44adc0de2990fa23cb9bcc1b3034ae60
Atul16093 <atulrag3@gmail.com>
2025-01-03 16:58:51 +0530

    Basic java Problem

---
+++ Problem Solving/Assignment1/Q40.java
@@ -0,0 +1,10 @@
+// Calculate the volume of the cylinder: r = 5 cm, h = 9 cm
+public class Q40 {
+    public static void main(String[] args) {
+        int r = 5;
+        int h = 9;
+        //calculate the volume of the cylinder 
+        double volume = Math.PI * r * r * h;
+        System.out.println("The volume of the cylinder is -: "+volume);
+    }
+}
